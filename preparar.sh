bef6231ba1e1b3a49b3f33458732c80db374fae3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-16 09:51:58 -0300

    Create preparar.sh

---
+++ preparar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+$ Instalação do Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo gpasswd -a $USER docker
+newgrp docker
+
+# Instalar o docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Instalação da AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Criar arquivo vazio das credenciais da AWS:
+mkdir ~/.aws
+touch ~/.aws/credentials
