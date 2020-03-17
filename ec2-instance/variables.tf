f7e735c60817189825099b9a1397183e6e45711a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-17 09:55:17 -0300

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -13,15 +13,12 @@ variable "key_name" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    eu-west-1 = "ami-674cbc1e"
-    us-east-1 = "ami-1d4e7a66"
-    us-west-1 = "ami-969ab1f6"
-    us-west-2 = "ami-8803e0f0"
+    us-east-1 = "ami-07ebfd5b3428b6f4d"
   }
 }
