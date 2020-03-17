9066f8e4e623451d9dccc70311c2d4f4f3f05e06
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-17 18:10:28 -0300

    Update preparar.sh

--- preparar.sh
+++ preparar.sh
@@ -3,6 +3,8 @@
 # --- DEV TOOLS
 # Instalacão do Java:
 # sudo apt update -y
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tJava:\n\n"
 sudo apt install -y default-jre
 sudo apt install -y default-jdk
 sudo update-alternatives -y --config java
@@ -15,6 +17,8 @@ END
 source /etc/environment
 
 # Instalação do Python:
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tPython:\n\n"
 sudo add-apt-repository universe
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-venv
@@ -22,15 +26,21 @@ sudo apt-get install -y python3-venv
 
 # --- OPS TOOLS
 # Instalação do Docker
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tDocker:\n\n"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo gpasswd -a $USER docker
 
 # Instalar o docker-compose
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tDocker-compose:\n\n"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Instalação da AWS CLI
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tAWS CLI e SSH:\n\n"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip
 unzip awscliv2.zip
@@ -45,6 +55,8 @@ echo 'ClientAliveInterval 60' | sudo tee --append /etc/ssh/sshd_config
 sudo service ssh restart
 
 # Instalação do Terraform:
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tTerraform:\n\n"
 curl "https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip" -o "terraform_0.12.23_linux_amd64.zip"
 unzip terraform_0.12.23_linux_amd64.zip
 mkdir ~/terraform
