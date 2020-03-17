4c4ada1f2c7059691145a16d5a0ef76067c4007f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-17 11:04:14 -0300

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -1,11 +1,10 @@
 variable "public_key_path" {
   description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-Example: ~/.ssh/terraform.pub
+Verificar a saida do comando: ssh-keygen -y -f ./chave-fiap.pem
+Exemplo: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41"
 DESCRIPTION
 }
+
 
 variable "key_name" {
   description = "chave-fiap.pem"
