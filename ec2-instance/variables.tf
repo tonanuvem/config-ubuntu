85d606fa657a8ec833d38585a8b63574aee5e86f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-18 00:37:10 -0300

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -1,18 +1,10 @@
-variable "public_key_path" {
-  default     = "public_key.pem"
-#  description = <<DESCRIPTION
-#Verificar a saida do comando: ssh-keygen -y -f ./chave-fiap.pem
-#Exemplo: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41"
-#DESCRIPTION
-}
-
 variable "instance_type" {
   # default     = "t2.micro"
   default     = "t2.medium"
 }
 
 variable "key_name" {
-  default     = "chave-fiap.pem"
+  default     = "chave-fiap"
 }
 
 variable "aws_region" {
@@ -26,3 +18,14 @@ variable "aws_amis" {
     us-east-1 = "ami-07ebfd5b3428b6f4d"
   }
 }
+
+# Usado para definir a chave com base na chave publica
+/*
+variable "public_key_path" {
+  default     = "public_key.pem"
+#  description = <<DESCRIPTION
+#Verificar a saida do comando: ssh-keygen -y -f ./chave-fiap.pem
+#Exemplo: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41"
+#DESCRIPTION
+}
+*/
