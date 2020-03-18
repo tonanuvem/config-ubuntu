33066a9bc653001e449c519f2c3b7b45b7f76d7b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-17 22:29:51 -0300

    Update main.tf

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,6 +1,6 @@
 # Define o provedor AWS onde serão criados os recursos
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 # Cria um VPC que receberá as instâncias e recursos
@@ -10,17 +10,17 @@ resource "aws_vpc" "default" {
 }
 # Cria um Internet Gateway que possibilita a comunicação do VPN com o mundo externo
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = aws_vpc.default.id
 }
 # Cria a Regra que permite acesso a Internet de/para o VPC
 resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  route_table_id         = aws_vpc.default.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
+  gateway_id             = aws_internet_gateway.default.id
 }
 # Cria uma subrede no VPC que ira receber as instâncias
 resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
+  vpc_id                  = aws_vpc.default.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
@@ -29,7 +29,7 @@ resource "aws_subnet" "default" {
 resource "aws_security_group" "default" {
   name        = "fiap-ec2-security-group-ec2-instance"
   description = "Grupo de seguranca do EC2"
-  vpc_id      = "${aws_vpc.default.id}"
+  vpc_id      = aws_vpc.default.id
 
   # Acesso SSH de qualquer um
   ingress {
@@ -65,16 +65,16 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_key_pair" "auth" {
-  key_name   = "${var.key_name}"
+  key_name   = var.key_name
   # executar o comando em sua maquina local: ssh-keygen -y -f ./chave-fiap.pem > public_key.pem
   # pegar o conteudo do arquivo e inserir no public_key.pem.
-  public_key = "${file(var.public_key_path)}"
+  public_key = file(var.public_key_path)
   # public_key = "$(var.public_key}"
 }
 
 # Para verificar o resultado do script: cat /var/log/cloud-init-output.log
 data "template_file" "init" {
-  template = "${file("${path.module}/../preparar.sh")}"
+  template = file("${path.module}/../preparar.sh")
 }
 
 resource "aws_instance" "web" {
@@ -83,12 +83,12 @@ resource "aws_instance" "web" {
   connection {
     # The default username for our AMI
     user = "ubuntu"
-    host = "${self.public_ip}"
+    host = self.public_ip
     # The connection will use the local SSH agent for authentication.
   }
   
   # Define o script de inicialização do EC2:
-  user_data = "${data.template_file.init.rendered}"
+  user_data = data.template_file.init.rendered
   
   # Define o nome da VM
   tags = {
@@ -96,25 +96,18 @@ resource "aws_instance" "web" {
   }  
   
   # Define tipo da VM (CPU e Memoria)
-  instance_type = "${var.instance_type}"
+  instance_type = var.instance_type
 
-  /*
-  # Criar um disco com 32 GB
-  ebs_block_device {
-    volume_size = 32
-    volume_type = "gp2"
-    device_name = "/dev/xvda"
-  } */
   # Criar um disco com 30 GB
   root_block_device {
     volume_size = 30
   }
   
   # Versão do Sistema Operacional (Ubuntu)
-  ami = "${lookup(var.aws_amis, var.aws_region)}"
+  ami = lookup(var.aws_amis, var.aws_region)
 
   # Chave: SSH keypair
-  key_name = "${aws_key_pair.auth.id}"
+  key_name = aws_key_pair.auth.id
 
   # Security group to allow HTTP and SSH access
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
@@ -122,20 +115,7 @@ resource "aws_instance" "web" {
   # We're going to launch into the same subnet as our ELB. In a production
   # environment it's more common to have a separate private subnet for
   # backend instances.
-  subnet_id = "${aws_subnet.default.id}"
-
-  # We run a remote provisioner on the instance after creating it.
-  # In this case, we just install nginx and start it. By default,
-  # this should be on port 80
-  /*
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get -y update",
-      "sudo apt-get -y install nginx",
-      "sudo service nginx start",
-    ]
-  }
-  */
+  subnet_id = aws_subnet.default.id
 
 # uso futuro: ELB
 /*
@@ -190,7 +170,6 @@ resource "aws_volume_attachment" "vol1" {
     volume_id = "${aws_ebs_volume.vol1.id}"
     device_name = "/dev/xvdb"
 }
-
 */
   
 }
