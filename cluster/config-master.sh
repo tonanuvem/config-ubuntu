72486b6e53b870615a3f8ffa0b5991df4309ed90
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-19 15:36:17 -0300

    Update config-master.sh

--- cluster/config-master.sh
+++ cluster/config-master.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tMASTER NODE: configurando hostname\n\n"
+
 sudo echo "hostname master" > /etc/profile
 sudo echo "export NICKNAME=master" > /etc/profile
