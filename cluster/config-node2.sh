2d5955ab19692702805defd85b089ee6861fce33
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-03-19 15:42:56 -0300

    Update config-node2.sh

--- cluster/config-node2.sh
+++ cluster/config-node2.sh
@@ -3,5 +3,5 @@
 printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
 printf "\n\n\tNODE 2: configurando hostname\n\n"
 
-sudo echo "hostname node2" > /etc/profile
-sudo echo "export NICKNAME=node2" > /etc/profile
+sudo echo "hostname node2" >> /etc/profile
+sudo echo "export NICKNAME=node2" >> /etc/profile
