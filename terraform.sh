dd984159f8bd1ac9797000c0666374c839d9d411
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-02 15:17:22 -0300

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -10,11 +10,11 @@ chmod +x /home/ubuntu/ip
 echo 'ClientAliveInterval 60' | sudo tee --append /etc/ssh/sshd_config
 sudo service ssh restart
 
-# Instalação do Terraform:
+# Instalação do Terraform: 
 printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
 printf "\n\n\tTerraform:\n\n"
-curl -s "https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip" -o "terraform_0.12.23_linux_amd64.zip"
-unzip terraform_0.12.23_linux_amd64.zip
+curl -s "https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip" -o "terraform_linux_amd64.zip"
+unzip terraform_linux_amd64.zip
 sudo mv terraform /usr/bin/
 
 # Verificando as versões instaladas e atualizar permissão docker:
