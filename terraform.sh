a4973ef0476acd14b55c703ab53f989cb14ccd0f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-02 15:13:34 -0300

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Diretorio escolhido para salvar os pacotes baixados
+cd /usr/local/src/
+# utils: cria script para verificar ip publico.
+sudo cat >> /home/ubuntu/ip <<EOL
+curl checkip.amazonaws.com
+EOL
+chmod +x /home/ubuntu/ip
+
 # Habilitar a configuração para o servidor não derrubar suas conexões:
 echo 'ClientAliveInterval 60' | sudo tee --append /etc/ssh/sshd_config
 sudo service ssh restart
