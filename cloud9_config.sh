654791afb264fda381e97d5e2f73efb12896a2e1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-06 14:04:09 -0300

    Update cloud9_config.sh

--- cloud9_config.sh
+++ cloud9_config.sh
@@ -50,7 +50,7 @@ sudo mv minikube /usr/bin/
 #echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 #curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 # sudo snap update
-sudo snap install kubeadm kubelet kubectl --classic
+sudo snap install kubectl --classic
 
 # helm
 # curl -s https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz -o helm-linux-amd64.tar.gz
