3462357df44e247376e7375c80e96eff812ac014
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-04-14 12:10:52 -0300

    Update cloud9.sh

--- cloud9.sh
+++ cloud9.sh
@@ -3,8 +3,8 @@
 # https://code.visualstudio.com/docs/remote/ssh
 
 # utils: cria script para verificar ip publico.
-touch /home/ubuntu/enviroment/ip
-sudo cat >> /home/ubuntu/enviroment/ip <<EOL
+touch ~/enviroment/ip
+sudo cat >> ~/enviroment/ip <<EOL
 curl checkip.amazonaws.com
 EOL
 chmod +x /home/ubuntu/enviroment/ip
