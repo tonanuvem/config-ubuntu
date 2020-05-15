b0a2e6979efc55d7feb9abe1be4a22300dff2eed
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-15 10:15:04 -0300

    Update firewall_allow.sh

--- firewall_allow.sh
+++ firewall_allow.sh
@@ -2,6 +2,6 @@ echo "O nome do grupo de seguranca será exibido a seguir, aguardar."
 echo ""
 aws ec2 describe-security-groups | grep GroupName
 echo ""
-echo "Copie e cole o nome do grupo de seguranca CLOUD9 exibido acima : " 
+echo "Copie e cole o nome do grupo de seguranca CLOUD9 exibido acima (começa com aws-cloud9-fiap): " 
 read NOME_GRUPO_SEGURANCA
 aws ec2 authorize-security-group-ingress --group-name $NOME_GRUPO_SEGURANCA --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
