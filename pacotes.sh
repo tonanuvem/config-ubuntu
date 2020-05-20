613b31deb54cdce010aca58a78eaf20e4033889e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-20 13:50:40 -0300

    Update pacotes.sh

--- pacotes.sh
+++ pacotes.sh
@@ -2,7 +2,7 @@
 
 echo "Aguardando Cloud9 : apt.systemd.daily update."
 while [ "$(ps aux | grep -i apt | wc -l)" != "1" ]; do
-  sleep 1 | echo "."
+  sleep 1 | echo -ne "."
 done
 echo "Instalando pacotes."
 sudo apt-get -y install jq maven build-essential zlib1g-dev libssl-dev libncurses-dev libffi-dev libsqlite3-dev libreadline-dev libbz2-dev > /dev/null
