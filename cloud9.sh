177a6e93f533fca8cfacdb4982c3a2b86b2d23c5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-20 11:08:41 -0300

    Update cloud9.sh

--- cloud9.sh
+++ cloud9.sh
@@ -9,12 +9,12 @@ EOL
 chmod +x ~/environment/ip
 
 # aumentando o disco para 20G e 
-sh ~/environment/config-ubuntu/resize.sh
+sh ~/environment/config-ubuntu/resize.sh > /dev/null
 #liberando acesso externo
 sudo apt-get -y install jq > /dev/null
 NOME_GRUPO_SEGURANCA=aws ec2 describe-security-groups | jq '.SecurityGroups[] | select(.GroupName | contains("cloud9")) | .GroupName'
 aws ec2 authorize-security-group-ingress --group-name $NOME_GRUPO_SEGURANCA --protocol tcp --port 0-65535 --cidr 0.0.0.0/0
-sh ~/environment/config-ubuntu/resize.sh
+sh ~/environment/config-ubuntu/resize.sh > /dev/null
 
 # --- DEV TOOLS
 # Instalacão do Maven Java:
