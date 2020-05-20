a5a677549eb94ed0f8b7d892b6e3b9437eccfb28
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-05-20 12:11:17 -0300

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -6,14 +6,14 @@ set -e
 # todo: add method for checking if latest or automatically grabbing latest
 mvn_version=${mvn_version:-3.6.3}
 url="http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/${mvn_version}/binaries/apache-maven-${mvn_version}-bin.tar.gz"
-install_dir="/opt/maven"
+install_dir="."
 
-if [ -d ${install_dir} ]; then
-    sudo mv ${install_dir} ${install_dir}.$(date +"%Y%m%d")
-fi
+#if [ -d ${install_dir} ]; then
+#    sudo mv ${install_dir} ${install_dir}.$(date +"%Y%m%d")
+#fi
 
-sudo mkdir ${install_dir}
-curl -fsSL ${url} | tar zx --strip-components=1 -C ${install_dir}
+#sudo mkdir ${install_dir}
+curl -fsSL ${url} | tar zx -C ${install_dir} # tar zx --strip-components=1 -C ${install_dir}
 
 #cat << EOF > /etc/profile.d/maven.sh
 sudo cat >> /etc/environment <<EOF
