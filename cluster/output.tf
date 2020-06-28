9246856604130f9b0fd131c518b7ef2bb4828d7f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-06-27 23:29:16 -0300

    Update output.tf

--- cluster/output.tf
+++ cluster/output.tf
@@ -1,11 +1,3 @@
-output "Master_dns_externo" {
-  value = "${aws_instance.master.public_dns}"
-}
-
-output "Master_ip_externo" {
-  value = "${aws_instance.master.public_ip}"
-}
-
 output "Node_1_dns_externo" {
   value = "${aws_instance.node1.public_dns}"
 }
@@ -20,4 +12,12 @@ output "Node_2_dns_externo" {
 
 output "Node_2_ip_externo" {
   value = "${aws_instance.node2.public_ip}"
+}
+
+output "Node_3_dns_externo" {
+  value = "${aws_instance.node3.public_dns}"
+}
+
+output "Node_3_ip_externo" {
+  value = "${aws_instance.node3.public_ip}"
 }
