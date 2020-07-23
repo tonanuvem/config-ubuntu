3adc686b572a832005fd23a5a76053c26ba4504e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-07-23 01:19:29 -0300

    Update run.sh

--- rancher/ingress-cloud9-nginx/run.sh
+++ rancher/ingress-cloud9-nginx/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Arquitetura Alta Disponibilidade:
+# https://rancher.com/docs/rancher/v2.x/en/installation/how-ha-works/
+
 # Existem varias opcoes de LoadBalancer. Nesse LAB, usaremos NGINX pela simplicidade.
 # https://rancher.com/docs/rancher/v2.x/en/installation/options/nginx/
 # https://rancher.com/docs/rancher/v2.x/en/installation/options/nlb/
@@ -9,7 +12,4 @@
 NODE_UM=$(grep -E ".*1=\d{" output.txt)
 #
 
-docker run -d --restart=unless-stopped \
-  -p 80:80 -p 443:443 \
-  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
-  nginx:alpine
+docker run -d --restart=unless-stopped   -p 800:80 -p 8443:443   -v $(pwd):/etc/nginx/ nginx:alpine
