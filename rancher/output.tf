ed9c11407e79a3f330c2d19a0222848312686237
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-07-22 23:36:41 -0300

    Update output.tf

--- rancher/output.tf
+++ rancher/output.tf
@@ -2,10 +2,10 @@ output "rancher_server_url" {
   value = module.rancher_common.rancher_url
 }
 
-output "rancher_node_ip" {
-  value = aws_instance.rancher_server.public_ip
+output "Node_1_ip_externo" {
+  value = aws_instance.quickstart_node.public_ip
 }
 
-output "workload_node_ip" {
-  value = aws_instance.quickstart_node.public_ip
+output "Node_2_ip_externo" {
+  value = aws_instance.quickstart_node_2.public_ip
 }
