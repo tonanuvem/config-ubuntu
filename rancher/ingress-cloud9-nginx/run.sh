f054ff47759ba19d96778df5c76c46a06d73eacb
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-07-23 00:58:32 -0300

    Update run.sh

--- rancher/ingress-cloud9-nginx/run.sh
+++ rancher/ingress-cloud9-nginx/run.sh
@@ -11,5 +11,5 @@ NODE_UM=$(grep -E ".*1=\d{" output.txt)
 
 docker run -d --restart=unless-stopped \
   -p 80:80 -p 443:443 \
-  -v ./nginx.conf:/etc/nginx/nginx.conf \
+  -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf \
   nginx:alpine
