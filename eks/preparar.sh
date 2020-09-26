d453c713b9f90a41a88ed1b9af887356a40c0590
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-26 16:14:32 -0300

    Update preparar.sh

--- eks/preparar.sh
+++ eks/preparar.sh
@@ -1,5 +1,12 @@
-aws iam create-role --role-name eksFiapClusterRole --assume-role-policy-document file://eksFiapClusterRole.json
+aws iam create-role --role-name eksFiapClusterRole
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy --role-name eksFiapClusterRole
 
-aws iam create-role --role-name eksFiapWoker --assume-role-policy-document file://eksFiapWorker.json
+#aws iam create-role --role-name eksFiapWoker --assume-role-policy-document file://eksFiapWorker.json
+aws iam create-role --role-name eksFiapWoker
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy --role-name eksFiapWoker
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy --role-name eksFiapWoker
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly --role-name eksFiapWoker
+
+
 
 aws eks --region us-east-1 update-kubeconfig --name eksfiap
