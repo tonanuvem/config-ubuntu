bbccb37548df5b1744277a716dcc5bdb8bd3a681
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-26 16:03:31 -0300

    Update preparar.sh

--- eks/preparar.sh
+++ eks/preparar.sh
@@ -1,5 +1,5 @@
 aws iam create-role --role-name eksFiapClusterRole --assume-role-policy-document file://eksFiapClusterRole.json
 
-aws iam create-role --role-name eksFiapWoker --assume-role-policy-document file://eksFiapWoker.json
+aws iam create-role --role-name eksFiapWoker --assume-role-policy-document file://eksFiapWorker.json
 
 aws eks --region us-east-1 update-kubeconfig --name eksfiap
