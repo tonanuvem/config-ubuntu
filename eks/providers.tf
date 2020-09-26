315187943cc606bbb15fc8ef65dc62b885a8eae3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-26 14:03:54 -0300

    Update providers.tf

--- eks/providers.tf
+++ eks/providers.tf
@@ -7,9 +7,3 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "available" {}
-
-# Not required: currently used in conjunction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
-provider "http" {}
