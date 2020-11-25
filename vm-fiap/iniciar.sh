963704e6f126e2210daffc706fa26fad20c62b4e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-11-25 10:37:58 -0300

    Update iniciar.sh

--- vm-fiap/iniciar.sh
+++ vm-fiap/iniciar.sh
@@ -1,6 +1,7 @@
 terraform init; terraform plan; terraform apply -auto-approve
 echo ""
 echo "Aguardando configurações "
+sleep 5
 export IP=$(terraform output ip_externo) && ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 #ssh -i "~/environment/chave-fiap.pem" ubuntu@$IP "while [ $(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 #ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(dpkg -l | grep kubeadm | wc -l) != '1' ]; do { printf .; sleep 1; } done"
