a1b43e40edd54a13864537f30214437a5a6439fb
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-21 21:46:07 -0300

    Update iniciar.sh

--- vm-fiap/iniciar.sh
+++ vm-fiap/iniciar.sh
@@ -1,7 +1,7 @@
 terraform init; terraform plan; terraform apply -auto-approve
 echo ""
 echo "   Aguardando configurações: "
-sleep 5
+sleep 10
 export IP=$(terraform output ip_externo)
 while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
