b363c5d02db47d4ce6f11c623b24f4a395136681
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-23 12:00:12 -0300

    Update iniciar.sh

--- istio-servicemesh/iniciar.sh
+++ istio-servicemesh/iniciar.sh
@@ -1,1 +1,9 @@
 terraform init; terraform plan; terraform apply -auto-approve
+echo ""
+echo "   Aguardando configurações: "
+sleep 10
+export IP=$(terraform output ip_externo)
+while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+echo "   Configuração OK."
