08708f77d2fb43acdf41db387f2adc121a0fb798
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-05 01:35:10 -0300

    Update iniciar.sh

--- istio-servicemesh/iniciar.sh
+++ istio-servicemesh/iniciar.sh
@@ -3,7 +3,7 @@ echo ""
 echo "   Aguardando configurações: "
 sleep 10
 export IP=$(terraform output ip_externo)
-while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep minikube | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 echo "   Configuração OK."
