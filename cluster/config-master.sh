6c7afbc12854a51d8d1445e2867ede51ff444a24
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-04 23:28:55 -0300

    Update config-master.sh

--- cluster/config-master.sh
+++ cluster/config-master.sh
@@ -9,7 +9,7 @@ sleep 10
 export IP=$(terraform output Node_1_ip_externo)
 while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+ssh -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 
 ssh -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname master"
 echo "   Configuração MASTER OK."
