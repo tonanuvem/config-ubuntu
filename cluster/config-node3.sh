a650d0e37daaea5bc65309916c29e6db70fea7c2
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-04 23:30:15 -0300

    Update and rename config-node2.sh to config-node3.sh

--- cluster/config-node3.sh
+++ cluster/config-node3.sh
@@ -9,7 +9,7 @@ sleep 1
 export IP=$(terraform output Node_3_ip_externo)
 while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+ssh -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 
 ssh -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname node2"
 echo "   Configuração NODE 2 OK."
