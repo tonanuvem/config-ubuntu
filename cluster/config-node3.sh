17a361c5d863962f1510fa460bcb2946e41b3896
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-05 00:52:49 -0300

    Update config-node3.sh

--- cluster/config-node3.sh
+++ cluster/config-node3.sh
@@ -7,7 +7,7 @@ echo ""
 echo "   Aguardando configurações: "
 sleep 1
 export IP=$(terraform output Node_3_ip_externo)
-while [ $(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
 ssh -o LogLevel=error -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 
