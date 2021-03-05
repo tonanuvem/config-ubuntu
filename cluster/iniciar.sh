c20ca8976f6c20862920a8391cdddb49608a13ad
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-04 23:17:10 -0300

    Update iniciar.sh

--- cluster/iniciar.sh
+++ cluster/iniciar.sh
@@ -1,9 +1,8 @@
 terraform init; terraform plan; terraform apply -auto-approve
 echo ""
-echo "   Aguardando configurações: "
-sleep 10
-export IP=$(terraform output ip_externo)
-while [ $(ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
-echo "   Conectado ao $IP, verificando ajustes: "
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
-echo "   Configuração OK."
+echo " Iniciando configurações: "
+echo ""
+sh config-master.sh
+sh config-node1.sh
+sh config-node2.sh
+echo " Cluster iniciado e configurado"
