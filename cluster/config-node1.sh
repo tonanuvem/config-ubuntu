15c9019335befe0be09260960bfa92f974cd6241
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-04 23:54:34 -0300

    Update config-node1.sh

--- cluster/config-node1.sh
+++ cluster/config-node1.sh
@@ -7,9 +7,9 @@ echo ""
 echo "   Aguardando configurações: "
 sleep 10
 export IP=$(terraform output Node_1_ip_externo)
-while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+while [ $(ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
-ssh -q -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+ssh -o LogLevel=error -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 
-ssh -q -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname node1"
+ssh -o LogLevel=error -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname node1"
 echo "   Configuração NODE 1 OK."
