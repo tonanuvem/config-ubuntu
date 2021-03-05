8b72d52625d0822fecf6f8b22a2e717e34424993
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-04 23:58:37 -0300

    Update conectar.sh

--- vm-fiap/conectar.sh
+++ vm-fiap/conectar.sh
@@ -3,4 +3,4 @@
 IP=$(terraform output ip_externo)
 
 echo "Conectando.. IP = $IP.."
-ssh -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP
+ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP
