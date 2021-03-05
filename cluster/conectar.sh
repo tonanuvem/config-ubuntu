df59b2014323375f26e52d044f7102c9d3da4905
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-05 00:22:32 -0300

    Update conectar.sh

--- cluster/conectar.sh
+++ cluster/conectar.sh
@@ -1,8 +1,14 @@
-echo "Os IPs das máquinas do Cluster serão exibidos a seguir, aguardar."
+#echo "Os IPs das máquinas do Cluster serão exibidos a seguir, aguardar."
 echo ""
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress, Tags[?Key=='Name'].Value|[0]]" --output text | grep -v None | grep cluster
-echo ""
-echo "Copie e cole um dos IPs exibidos acimas : " 
-read IP
+#aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress, Tags[?Key=='Name'].Value|[0]]" --output text | grep -v None | grep cluster
+#echo ""
+#echo "Copie e cole um dos IPs exibidos acimas : " 
+#read IP
+
+echo "Em qual NODE você deseja conectar? Digitar: 1 ou 2 ou 3" 
+read NODENUM
+NODE="Node_${NODENUM}_ip_externo"
+export IP=$(terraform output $NODE)
+
 echo "Conectando.. IP = $IP.."
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP
