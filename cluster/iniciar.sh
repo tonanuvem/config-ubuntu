ce6e44f4d2125be2a232e7c93b0f2f40c135d5b8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-05 00:02:14 -0300

    Update iniciar.sh

--- cluster/iniciar.sh
+++ cluster/iniciar.sh
@@ -1,7 +1,6 @@
 terraform init; terraform plan; terraform apply -auto-approve
 echo ""
-echo " Iniciando configurações: "
-echo ""
+echo "Iniciando configurações: "
 sh config-node1.sh
 sh config-node2.sh
 sh config-node3.sh
