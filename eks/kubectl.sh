a692203e53f25faeeb0b1cb88bf61f590a040077
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-05 23:20:15 -0300

    Update kubectl.sh

--- eks/kubectl.sh
+++ eks/kubectl.sh
@@ -1,1 +1,1 @@
-docker run -ti -v ~/.aws/credentials:/root/.aws/credentials -v ~/.kube/config:/root/.kube.config/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
+docker run -ti -v ~/.aws/:/root/.aws/ -v ~/.kube/:/root/.kube/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
