13f3bd20c81530a1e50778e2004a5e0c560485f5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-06 00:07:26 -0300

    Update kubectl.sh

--- eks/kubectl.sh
+++ eks/kubectl.sh
@@ -1,1 +1,12 @@
-docker run -ti -v ~/.aws/:/root/.aws/ -v ~/.kube/:/root/.kube/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-kubectl-binary-with-curl-on-linux
+# https://github.com/infrastructure-as-code/docker-aws-cli/blob/master/aws.sh
+# docker run -ti -v ~/.aws/:/root/.aws/ -v ~/.kube/:/root/.kube/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
+
+docker run -ti \
+	-e "AWS_ACCESS_KEY_ID=${aws_access_key_id}" \
+	-e "AWS_SECRET_ACCESS_KEY=${aws_secret_access_key}" \
+	-e "AWS_DEFAULT_REGION=us-east-1" \
+	--entrypoint /bin/sh -d tonanuvem/kubectl-aws-cli
+
+docker exec -ti kubectl aws eks --region us-east-1 update-kubeconfig --name eksfiap
+docker exec -ti kubectl /bin/sh && mkdir ~/fiap && cd ~/fiap
