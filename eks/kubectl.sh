51ab5b320f49fc16984bbd1f8fbae0cbb4ff2c1c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-07 14:54:59 -0300

    Update kubectl.sh

--- eks/kubectl.sh
+++ eks/kubectl.sh
@@ -3,7 +3,7 @@
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-kubectl-binary-with-curl-on-linux
 # https://github.com/infrastructure-as-code/docker-aws-cli/blob/master/aws.sh
 
-read -d '' CRED <<fim
+read -d '' x <<EOF
 echo "$CRED" >> ./files/credentials
 
 # docker run -ti --rm --name kubectl -v ~/.aws/:/root/.aws/ -v ~/.kube/:/root/.kube/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
