4f278471309f288afe0091a7c67c480fc21bbd4d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-07 13:35:39 -0300

    Update conectar.sh

--- shift/conectar.sh
+++ shift/conectar.sh
@@ -1,7 +1,7 @@
 echo ""
 echo "Os IPs das máquinas do Cluster serão exibidos a seguir, aguardar."
 echo ""
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress, Tags[?Key=='Name'].Value|[0]]" --output text | grep -v None | grep vm
+aws ec2 describe-instances --query "Reservations[*].Instances[*].[PublicIpAddress, Tags[?Key=='Name'].Value|[0]]" --output text | grep -v None | grep vm_
 echo ""
 echo "Copie e cole um dos IPs exibidos acimas : " 
 read IP
