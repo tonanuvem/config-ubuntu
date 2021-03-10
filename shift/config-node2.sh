c32ea9953534ea90813a3247c1e6033c0367eb18
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 09:34:49 -0300

    Create config-node2.sh

---
+++ shift/config-node2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+printf "\n\n xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n"
+printf "\n\n\tNODE 2: configurando hostname\n\n"
+
+echo ""
+echo "   Aguardando configurações: "
+sleep 1
+IP=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_2/) ) { print $1} }')
+while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
+echo "   Conectado ao $IP, verificando ajustes: "
+ssh -o LogLevel=error -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
+
+ssh -o LogLevel=error -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname node2"
+echo "   Configuração NODE 2 OK."
