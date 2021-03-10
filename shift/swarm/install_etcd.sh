5ff919ce64a59e28d465ff4e8243d9506f07ae7c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 12:03:34 -0300

    Update install_etcd.sh

--- shift/swarm/install_etcd.sh
+++ shift/swarm/install_etcd.sh
@@ -14,7 +14,7 @@ docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380
  -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
  -initial-advertise-peer-urls http://${HostIP}:2380 \
  -listen-peer-urls http://0.0.0.0:2380 \
- -initial-cluster-token etcd-cluster-1 \
+ -initial-cluster-token FIAP \
  -initial-cluster etcd0=http://${HostIP}:2380 \
  -initial-cluster-state new
 
