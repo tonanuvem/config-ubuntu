8c2194a48e7c4ce339ef4f917afc8f8fb3237335
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 10:51:23 -0300

    Update iniciar.sh

--- shift/iniciar.sh
+++ shift/iniciar.sh
@@ -1,9 +1,1 @@
 terraform init; terraform plan; terraform apply -auto-approve
-echo ""
-echo " Iniciando configurações: "
-sh config-master.sh
-sh config-node1.sh
-sh config-node2.sh
-sh config-node3.sh
-echo ""
-echo " Master e Nodes 1, 2, 3 iniciados e configurados"
