5c540fe9d7838869855326f2f3d97abc08b7711f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 09:53:22 -0300

    Update config-master.sh

--- shift/config-master.sh
+++ shift/config-master.sh
@@ -11,5 +11,5 @@ while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubun
 echo "   Conectado ao $IP, verificando ajustes: "
 ssh -o LogLevel=error -i ~/environment/chave-fiap.pem ubuntu@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
 
-ssh -o LogLevel=error -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname node3"
+ssh -o LogLevel=error -i "~/environment/chave-fiap.pem" ubuntu@$IP "sudo hostnamectl set-hostname master"
 echo "   Configuração MASTER OK."
