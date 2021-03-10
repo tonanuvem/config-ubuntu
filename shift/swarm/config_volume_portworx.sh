42c3aadf0dd97e7e059307de66247215328fd726
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 10:43:03 -0300

    Create config_volume_portworx.sh

---
+++ shift/swarm/config_volume_portworx.sh
@@ -0,0 +1,27 @@
+# Cada Worker possui um disco extra (/dev/xvdb) criado pelo Terraform:
+
+echo "Configuraçoes de disco: "
+lsblk
+
+# https://docs.portworx.com/install-with-other/docker/swarm/
+REL="/2.6"  # Portworx v2.6 release
+
+latest_stable=$(curl -fsSL "https://install.portworx.com$REL/?type=dock&stork=false&aut=false" | awk '/image: / {print $2}')
+
+# Download Bundle (reminder, you will still need to run `px-runc install ..` after this step)
+sudo docker run --entrypoint /runc-entry-point.sh \
+    --rm -i --privileged=true \
+    -v /opt/pwx:/opt/pwx -v /etc/pwx:/etc/pwx \
+    $latest_stable
+
+# RUN "px-runc install" command from the bundle to configure your installation
+sudo /opt/pwx/bin/px-runc install -c FIAP_CLUSTER \
+    -k etcd://localhost:2379 \
+    -s /dev/xvdb -s /dev/xvdc
+
+# Verificar status:
+echo "Verificando status : "
+pxctl status
+
+#PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
+#docker exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl status
