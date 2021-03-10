e0145fbff6690d8e23b0cd70f1c51a52e0e4e029
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-10 09:48:36 -0300

    Update conectar.sh

--- shift/conectar.sh
+++ shift/conectar.sh
@@ -7,5 +7,5 @@
 #read IP
 
 MASTER=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
-echo "Conectando.. IP = $MASTER.."
+echo "Conectando.. IP = $MASTER"
 ssh -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ubuntu@$MASTER
