6c3d0629dc1ed83852f0f1a338095f7d223ee7c1
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-03-12 00:37:15 -0300

    Update kubectl.sh

--- eks/kubectl.sh
+++ eks/kubectl.sh
@@ -3,11 +3,13 @@
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-kubectl-binary-with-curl-on-linux
 # https://github.com/infrastructure-as-code/docker-aws-cli/blob/master/aws.sh
 
-echo "Cole suas credenciais da AWS, e digite ENTER:"
-CRED=$(sed '/^$/q')
+if [[ -f "./files/credentials" && -s "./files/credentials ]]; then 
+    echo "Arquivo com credenciais encontrado"
+else 
+    echo "Credenciais nao encontrada"; 
+    sh config_credenciais.sh
+fi
 
-#read -d '' x <<EOF
-echo "$CRED" > ./files/credentials
 
 # docker run -ti --rm --name kubectl -v ~/.aws/:/root/.aws/ -v ~/.kube/:/root/.kube/ --entrypoint /bin/sh tonanuvem/kubectl-aws-cli
 echo ""
