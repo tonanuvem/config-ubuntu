765fd454ed92214fed28ed1ca88d1e633002df95
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-08-25 18:00:56 -0300

    Update iniciar.sh

--- aws-vm/iniciar.sh
+++ aws-vm/iniciar.sh
@@ -2,7 +2,7 @@ terraform init; terraform plan; terraform apply -auto-approve
 echo ""
 echo "   Aguardando configurações: "
 sleep 10
-export IP=$(terraform output ip_externo)
+export IP=$(~/environment/ip | awk -Fv '{ if ( !($1 ~  "None") && (/vm_0/) ) { print $1} }')
 while [ $(ssh -q -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ec2-user@$IP "echo CONECTADO" | grep CONECTADO | wc -l) != '1' ]; do { printf .; sleep 1; } done
 echo "   Conectado ao $IP, verificando ajustes: "
 #ssh  -o LogLevel=error -oStrictHostKeyChecking=no -i ~/environment/chave-fiap.pem ec2-user@$IP "while [ \$(ls /usr/local/bin/ | grep docker-compose | wc -l) != '1' ]; do { printf .; sleep 1; } done"
